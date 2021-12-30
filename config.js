140fa19039973676ad72ebfb723dec29d51322b4
Paul Franz <paulkfranz@gmail.com>
2021-12-30 08:11:13 -0500

    new layers, data

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/daltonwb/ckxjavthg1mw514nomso4vc6a',
+    style: 'mapbox://styles/daltonwb/ckxsaz25s1dx414qdkmb9lz6u',
     accessToken: 'pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAOAncwelv9Vw',
     showMarkers: false,
     markerColor: '#3FB1CE',
@@ -18,19 +18,37 @@ var config = {
           image: '',
           description: 'Since 2019, maritime tensions around involving Iran  have grown more pronounced, with dozens of confirmed or suspected incidents in a multidimensional shadow war playing out from the Indian Ocean to the Mediterranean.',
           location: {
-            center: [12.234568, 23.422547],
-            zoom: 2,
-            pitch: 0,
-            bearing: 0
+            center: [56.227470, 23.626711],
+            zoom: 3,
+            pitch: 35,
+            bearing: -12
           },
           mapAnimation: 'flyTo',
           rotateAnimation: false,
           callback: '',
           onChapterEnter: [
-
+            {
+              layer: 'countries',
+              duration: 1000,
+              opacity: .6
+            },
+            {
+              layer: 'countries-labels',
+              duration: 1000,
+              opacity: 1
+            }
           ],
           onChapterExit: [
-
+            {
+              layer: 'countries',
+              duration: 1000,
+              opacity: 0
+            },
+            {
+              layer: 'countries-labels',
+              duration: 1000,
+              opacity: 0
+            }
         ]
       },
         {
@@ -41,19 +59,47 @@ var config = {
             image: '',
             description: 'Nearly one-fifth of the world’s oil, and a quarter of its liquified natural gas, flows through the Strait of Hormuz. More than forty thousand ships each year. But this chokepoint and the waters around it are also a major geopolitical flashpoint.',
             location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
-              pitch: 0,
-              bearing: 0
+              center: [57.210231, 24.266239],
+              zoom: 5.4,
+              pitch: 47,
+              bearing: -8.8
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-
+              {
+                layer: 'hormuz',
+                duration: 1000,
+                opacity: 1
+              },
+              {
+                layer: 'natural-label',
+                duration: 1000,
+                opacity: 1
+              },
+              {
+                layer: 'hormuz-dot',
+                duration: 1000,
+                opacity: 1
+              }
             ],
             onChapterExit: [
-
+              {
+                layer: 'hormuz',
+                duration: 1000,
+                opacity: 0
+              },
+              {
+                layer: 'hormuz-dot',
+                duration: 1000,
+                opacity: 0
+              },
+              {
+                layer: 'natural-label',
+                duration: 1000,
+                opacity: 0
+              }
           ]
         },
         {
@@ -64,8 +110,8 @@ var config = {
             image: '',
             description: 'The implications of an escalation would be profound and carry global reverberations.',
             location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
+              center: [48.000236, 23.802554],
+              zoom: 4.25,
               pitch: 0,
               bearing: 0
             },
@@ -73,17 +119,25 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-
+              {
+                layer: 'maritime-tracker',
+                duration: 1000,
+                opacity: 1
+              }
             ],
             onChapterExit: [
-
+              {
+                layer: 'maritime-tracker',
+                duration: 1000,
+                opacity: 0
+              }
           ]
         },
         {
             id: '03',
             alignment: 'left',
             hidden: false,
-            title: 'Commercial tanker interception',
+            title: '',
             image: '',
             description: 'A spate of attacks against commercial tankers in May 2019 marked the start of a two-pronged Iranian response to the U.S. “maximum pressure” campaign, which significantly interrupted Iran’s oil export, a key revenue source for Tehran.',
             location: {
@@ -150,11 +204,11 @@ var config = {
         },
         {
             id: '06',
-            alignment: 'left',
+            alignment: 'full',
             hidden: false,
-            title: 'Commercial tanker interception',
+            title: '',
             image: '',
-            description: '“We will make the enemy understand that either everyone can use the Strait of Hormuz or no one”, said the commander of the Islamic Revolutionary Guard Corps (IRGC) in July 2018.',
+            description: '<h2 class="liftout">“We will make the enemy understand that either everyone can use the Strait of Hormuz or no one”,</h2> – Commander of the Islamic Revolutionary Guard Corps (IRGC) in July 2018.',
             location: {
               center: [12.234568, 23.422547],
               zoom: 2,
@@ -172,12 +226,12 @@ var config = {
           ]
         },
         {
-            id: '07',
-            alignment: 'left',
+            id: '07-QUOTE',
+            alignment: 'full',
             hidden: false,
             title: '',
             image: '',
-            description: 'President Hassan Rouhani likewise warned that “America should know that we are selling our oil and will continue to sell our oil and they are not able to stop our oil exports… if tone day they want to prevent the export of Iran’s oil then no oil will be exported from the Persian Gulf”.',
+            description: '<h2 class="liftout">“America should know that we are selling our oil and will continue to sell our oil and they are not able to stop our oil exports… if one day they want to prevent the export of Iran’s oil then no oil will be exported from the Persian Gulf”.</h2>- Iranian President Hassan Rouhani',
             location: {
               center: [12.234568, 23.422547],
               zoom: 2,
@@ -198,23 +252,31 @@ var config = {
             id: '08',
             alignment: 'left',
             hidden: false,
-            title: '',
+            title: 'The port of Fujairah',
             image: '',
-            description: 'In 12 May 2019, amidst high tension between Tehran and Washington, four tankers were attacked of the UAE port of Fujairah.',
+            description: 'In 12 May 2019, amidst high tension between Tehran and Washington, four tankers were attacked near the UAE port of Fujairah.',
             location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
-              pitch: 0,
-              bearing: 0
+              center: [56.464665, 25.188390],
+              zoom: 11.9,
+              pitch: 63,
+              bearing: -109.30
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-
+              {
+                layer: 'fujairah-ships',
+                duration: 1000,
+                opacity: 1
+              }
             ],
             onChapterExit: [
-
+              {
+                layer: 'fujairah-ships',
+                duration: 1000,
+                opacity: 0
+              }
           ]
         },
         {
@@ -225,16 +287,20 @@ var config = {
             image: '',
             description: 'The Fujairah attacks were followed by a series of additional attacks and seizures against commercial shipping. By August 2021, there were at least 37 suspected Iranian operations against vessels flagged from more than a dozen countries.',
             location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
-              pitch: 0,
-              bearing: 0
+              center: [58.783763, 22.493531],
+              zoom: 5.6,
+              pitch: 31,
+              bearing: -9.77
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-
+              {
+                layer: 'maritime-tracker',
+                duration: 1000,
+                opacity: 1
+              }
             ],
             onChapterExit: [
 
@@ -244,9 +310,9 @@ var config = {
             id: '10',
             alignment: 'left',
             hidden: false,
-            title: 'Commercial tanker interception',
-            image: '',
-            description: 'In response, the U.S. announced it was putting together an International Maritime Security Construct to promote freedom of navigation. Eight countries are currently part of the Bahrain-based IMSC, which operates Task Force Sentinel. ',
+            title: '',
+            image: 'images/imsc_logo.png',
+            description: 'In response, the U.S. announced it was putting together an International Maritime Security Construct to promote freedom of navigation. Eight countries are currently part of the Bahrain-based IMSC, which operates Task Force Sentinel: <ul><li>Albania</li><li>Bahrain</li><li>Estonia</li><li>Lithuania</li><li>Saudi Arabia</li><li>United Arab Emirates</li><li>United Kingdom</li><li>United States</li>',
             location: {
               center: [12.234568, 23.422547],
               zoom: 2,
@@ -268,8 +334,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '',
-            image: '',
-            description: 'In January 2020, several European governments set up a parallel mission, European Maritime Awareness in the Strait of Hormuz (EMASOH), headquartered in Abu Dhabi.',
+            image: 'images/emasoh_logo.jpeg',
+            description: 'In January 2020, several European governments set up a parallel mission, European Maritime Awareness in the Strait of Hormuz (EMASOH), headquartered in Abu Dhabi.<ul><li>Belgium</li><li>Denmark</li><li>France</li><li>Germany</li><li>Greece</li><li>Italy</li><li>The Netherlands</li><li>Portugal</li>',
             location: {
               center: [12.234568, 23.422547],
               zoom: 2,
@@ -314,7 +380,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '',
-            image: '',
+            image: 'images/wsj.jpg',
             description: 'In March 2021, the WSJ revealed that in fact, since 2019 at least a dozen attacks had been carried out by Israel against Iranian vessels.',
             location: {
               center: [12.234568, 23.422547],
@@ -359,20 +425,24 @@ var config = {
             id: '15',
             alignment: 'left',
             hidden: false,
-            title: '',
-            image: '',
-            description: 'A July 2021 attack against the Mercer Street, a Liberian-flagged, Japanese-owned vessel linked to Israeli ownership, was a notable escalation. It was deadly, killing the ship’s Romanian master and a UK citizen. And it was widely attributed to Iran, including by the G-7 and EU.',
+            title: 'Attack on Oil Tanker',
+            image: 'images/mercer_street.jpg',
+            description: 'A July 2021 attack against the Mercer Street, a Liberian-flagged, Japanese-owned vessel linked to Israeli ownership, was a notable escalation. It was deadly, killing the ship’s Romanian master and a UK citizen. And it was widely attributed to Iran, including by the G-7 and EU. And it was widely attributed to Iran, including by the G-7 and EU. But Tehran dismissed what it called "bogus allegations".',
             location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
-              pitch: 0,
-              bearing: 0
+              center: [59.522652, 21.139930],
+              zoom: 9.45,
+              pitch: 56,
+              bearing: -20.36
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [
-
+              {
+                layer: 'maritime-tracker',
+                duration: 1000,
+                opacity: 1
+              }
             ],
             onChapterExit: [
 
@@ -380,29 +450,6 @@ var config = {
         },
         {
             id: '15',
-            alignment: 'left',
-            hidden: false,
-            title: '',
-            image: '',
-            description: 'A July 2021 attack against the Mercer Street, a Liberian-flagged, Japanese-owned vessel linked to Israeli ownership, was a notable escalation. It was deadly, killing the ship’s Romanian master and a UK citizen. And it was widely attributed to Iran, including by the G-7 and EU. But Tehran dismissed what it called "bogus allegations".',
-            location: {
-              center: [12.234568, 23.422547],
-              zoom: 2,
-              pitch: 0,
-              bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-
-            ],
-            onChapterExit: [
-
-          ]
-        },
-        {
-            id: '16',
             alignment: 'left',
             hidden: false,
             title: 'Policy Recommendations',
